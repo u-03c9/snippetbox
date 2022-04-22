7fb7ef7526681a14551e96b720acad855fbce454
Peter Khalil <uni.03c9@gmail.com>
2022-04-23 00:01:03 +0200

    connect to mysql and create new snippet

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,15 +1,20 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/u-03c9/snippetbox/pkg/models/mysql"
 )
 
 type application struct {
 	errorLog *log.Logger
 	infoLog  *log.Logger
+	snippets *mysql.SnippetModel
 }
 
 func getAddress() string {
@@ -25,13 +30,40 @@ func getAddress() string {
 	return address
 }
 
+func getDsn() string {
+	dbString, exist := os.LookupEnv("DSN")
+	if !exist {
+		dbString = "web:pass@/snippetbox?parseTime=true"
+	}
+	return dbString
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err
+	}
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
+
 func main() {
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stdout, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
 
+	db, err := openDB(getDsn())
+	if err != nil {
+		errorLog.Fatal(err)
+	}
+	defer db.Close()
+
 	app := &application{
 		errorLog: errorLog,
 		infoLog:  infoLog,
+		snippets: &mysql.SnippetModel{DB: db},
 	}
 
 	server := &http.Server{
@@ -41,6 +73,6 @@ func main() {
 	}
 
 	infoLog.Printf("Starting server on %s", server.Addr)
-	err := server.ListenAndServe()
+	err = server.ListenAndServe()
 	errorLog.Fatal(err)
 }
---
+++ pkg/models/mysql/snippets.go
@@ -0,0 +1,35 @@
+package mysql
+
+import (
+	"database/sql"
+
+	"github.com/u-03c9/snippetbox/pkg/models"
+)
+
+type SnippetModel struct {
+	DB *sql.DB
+}
+
+func (m *SnippetModel) Insert(title, content, expires string) (int, error) {
+	stmt := `INSERT INTO snippets (title, content, created, expires)
+	VALUES(?, ?, UTC_TIMESTAMP(), DATE_ADD(UTC_TIMESTAMP(), INTERVAL ? DAY))`
+
+	result, err := m.DB.Exec(stmt, title, content, expires)
+	if err != nil {
+		return 0, nil
+	}
+
+	id, err := result.LastInsertId()
+	if err != nil {
+		return 0, err
+	}
+	return int(id), nil
+}
+
+func (m *SnippetModel) Get(id int) (*models.Snippet, error) {
+	return nil, nil
+}
+
+func (m *SnippetModel) Latest() ([]*models.Snippet, error) {
+	return nil, nil
+}
